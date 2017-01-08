478c90a63763255e731914ef76e83fab55cf45d3
badking01 <mio.polini@gmail.com>
2017-01-08 22:05:26 +0700

    Update testuart.c

--- testuart.c
+++ testuart.c
@@ -1,39 +1,37 @@
 void json_Uart(void)
-	{
-			static char key;static char Buff_json[50];static uint8_t len=0;static bool json_start; static bool json_enable;
+	{static char key;static char Buff_json[50];static uint8_t len=0;static bool json_start; static bool json_enable;
   if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET) // Neu co ky tu moi trong bo dem nhan
    {
-		 key=(char)USART_ReceiveData(USART1);//Buff_json[len]=key;len++;
-		 if(key=='{')
-				{
-				 json_start=1;
-				}
-			if(json_start)
-		{				
-				switch(key)
-				{
-					case '{':	
-										{ 
-											Buff_json[0]=key;len=1;
-											break;
-										}
-					case '}':
-										{
-											json_start=0;
-											Buff_json[len]=key;
-											len++;
-											json_enable=1;
-											break;
-										}
-					default: 
-										{
-											Buff_json[len]=key;
-											len++;
-											break;
-										}
-				}
-		}
-   
-   }		
-	USART_ClearITPendingBit(USART1, USART_IT_RXNE);
+	  key=(char)USART_ReceiveData(USART1);//Buff_json[len]=key;len++;
+	  if(key=='{')
+	  {
+		  json_start=1;
+	  }
+	  if(json_start)
+	  {
+		  switch(key)
+		  {
+			  case '{':
+				  { 
+					  Buff_json[0]=key;len=1;
+					  break;
+				  }
+			  case '}':
+				  {
+					  json_start=0;
+					  Buff_json[len]=key;
+					  len++;
+					  json_enable=1;
+					  break;
+				  }
+			  default: 
+				  {
+					  Buff_json[len]=key;
+					  len++;
+					  break;
+				  }
+		  }
+	  }
+  }
+	 USART_ClearITPendingBit(USART1, USART_IT_RXNE);
 }
